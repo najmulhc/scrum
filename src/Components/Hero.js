aa2185242c813d63088978e8fb21c1c2eb178d3f
kridi32 <kridi32@outlook.com>
2022-06-29 14:44:21 +0600

    hero image placed in position

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,14 +1,20 @@
 import React from "react";
-import hero from '../Images/Hero.png'
+import hero from "../Images/Hero.png";
 import Navbar from "./Navbar";
 const Hero = () => {
   return (
-    <section className="hero">
+    <section className="hero relative ">
       <Navbar></Navbar>
-      <div>
-        <div></div>
-        <img src={hero} alt="" />
+      <div className="flex justify-between container mx-auto  ">
+        <div  >
+          <h1>this </h1>
+        </div>
+        <div>
+ 
+        </div>
+
       </div>
+      <img src={hero} className="absolute left-[1090px] top-[300px] scale-125 m-0 p-0"  alt="" />
     </section>
   );
 };
